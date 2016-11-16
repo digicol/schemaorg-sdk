a441a695b389fb613c81b22a81ccc23ae3cafd78
Tim Strehle <tim@strehle.de>
2016-11-16 15:41:06 +0100

    Utils::getStartPage() removed, Utils::getItemsPerPage() changed to "opensearch:itemsPerPage", Utils::getStartIndex() removed pagesize parameter.

--- src/Utils.php
+++ src/Utils.php
@@ -142,27 +142,12 @@ class Utils
      */
     public static function getItemsPerPage(array $input_properties, $default_pagesize)
     {
-        if (! isset($input_properties[ 'opensearch:count' ]))
+        if (! isset($input_properties[ 'opensearch:itemsPerPage' ]))
         {
             return $default_pagesize;
         }
 
-        return max(1, intval($input_properties[ 'opensearch:count' ]));
-    }
-
-
-    /**
-     * @param array $input_properties
-     * @return int
-     */
-    public static function getStartPage(array $input_properties)
-    {
-        if (! isset($input_properties[ 'opensearch:startPage' ]))
-        {
-            return 1;
-        }
-
-        return max(1, intval($input_properties[ 'opensearch:startPage' ]));
+        return max(1, intval($input_properties[ 'opensearch:itemsPerPage' ]));
     }
 
 
@@ -171,16 +156,13 @@ class Utils
      * @param int $default_pagesize
      * @return int
      */
-    public static function getStartIndex(array $input_properties, $default_pagesize)
+    public static function getStartIndex(array $input_properties)
     {
-        return
-            (
-                (
-                    self::getItemsPerPage($input_properties, $default_pagesize)
-                    * (self::getStartPage($input_properties) - 1)
-                )
-                + 1
-            );
-    }
+        if (! isset($input_properties[ 'opensearch:startIndex' ]))
+        {
+            return 1;
+        }
 
+        return max(1, intval($input_properties[ 'opensearch:startIndex' ]));
+    }
 }
